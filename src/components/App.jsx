8dc1a09522fd401ba9829e38470a8e90217d85e9
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2022-12-03 17:08:57 +0200

    add task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import React from "react";
+import { useState } from "react";
+
 export const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    setCounter(counter + 2);
+  };
+
+  const decrement = () => {
+    setCounter(counter - 1);
+  };
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Counter: {counter}</h1>
+      <button className="btn btn-success" onClick={increment}>
+        Add
+      </button>
+      <button className="btn btn-danger" onClick={decrement}>
+        Delete
+      </button>
     </div>
   );
 };
