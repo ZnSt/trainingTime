1166005bb842ce704fa5a37c231a46e34d011704
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2023-02-03 09:49:34 +0200

    update branch hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,44 +1,8 @@
-import { Component } from "react";
-
-import data from "../json/painting.json";
-import events from "../json/upcoming-events.json";
-import colorPickerOptions from "../json/colorPickerOptions";
-
-import { Counter } from "./Counter";
-import { Card } from "./Card";
-import { Section } from "./Section";
-import { PageTitle } from "./Calendar/PageTitle";
-import { EventBoard } from "./Calendar/EventsBoard";
-
-// State and SetState
-import { StillCounter } from "./StillCounter";
-import { Dropdown } from "./Dropdown";
-import { Colorpicker } from "./Colorpicker";
-import { TodoList } from "./ToDo/TodoList";
 import { Form } from "./Form";
-export class App extends Component {
-  formSubmitHandler = (data) => {
-    console.log(data);
-  };
-  render() {
-    return (
-      <div>
-        <Section title="Top Week" styles={{ textAlign: "center" }}>
-          <Card data={data} />
-          <Counter />
-        </Section>
-
-        <PageTitle title="24th Core Worlds Coalitions Conference" />
-        <EventBoard events={events} />
-
-        <StillCounter initialValue={0} />
-        <Dropdown />
-        <Colorpicker colors={colorPickerOptions} />
-
-        <TodoList />
-
-        {/* <Form onSubmit={this.formSubmitHandler} /> */}
-      </div>
-    );
-  }
-}
+export const App = () => {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+};
